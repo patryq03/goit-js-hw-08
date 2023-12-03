fe4e73026d3e3885d788b973d9349a6e17f8f927
patryq03 <138616976+patryq03@users.noreply.github.com>
2023-12-03 20:00:24 +0100

    feat: change src folder

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Vimeo from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+player.on('timeupdate', (event) => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const time = localStorage.getItem('videoplayer-current-time');
+    player.setCurrentTime(time);
+});
+
 
