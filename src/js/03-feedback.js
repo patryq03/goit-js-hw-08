7c7ce87625efa6f22271ceb4ebd4c82567736f05
patryq03 <138616976+patryq03@users.noreply.github.com>
2023-12-09 10:46:14 +0100

    feat: change src folder

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+const form = document.querySelector("form.feedback-form");
+const submit = document.querySelector("button");
+const email = document.querySelector("input").value;
+const message = document.querySelector("textarea").value;
 
+
+
+form.addEventListener("input", event => {
+    localStorage.setItem("feedback-form-state", event.target.value);
+});
+
+form.addEventListener("message", event => {
+    localStorage.setItem("feedback-form-state", event.target.value);
+})
+form.addEventListener("submit", event => {
+    event.preventDefault;
+    console.log(event.target);
+    localStorage.removeItem("feedback-form-state");
+    form.reset();
+})
