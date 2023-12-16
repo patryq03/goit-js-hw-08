df50bbac74a3f867c4ac0b9d1312f43b40fd52ea
patryq03 <138616976+patryq03@users.noreply.github.com>
2023-12-16 18:02:09 +0100

    feat: change src folder

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,14 @@
 import Vimeo from '@vimeo/player';
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo(iframe);
-player.on('timeupdate', throttle((event) => {
+player.on(
+  'timeupdate',
+  throttle(event => {
     localStorage.setItem('videoplayer-current-time', event.seconds);
-},1000));
+  }, 1000)
+);
 
 /*window.addEventListener('DOMContentLoaded', () => {
   const time = localStorage.getItem('videoplayer-current-time');
@@ -17,17 +20,13 @@ const storedTime = localStorage.getItem('videoplayer-current-time');
 if (storedTime) {
   player
     .setCurrentTime(storedTime)
-    .then(function (seconds) {
-      // seconds = the actual time that the player seeked to
-    })
+    .then(function (seconds) {})
     .catch(function (error) {
       switch (error.name) {
         case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
           break;
 
         default:
-          // some other error occurred
           break;
       }
     });
