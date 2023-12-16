0ca9e1d90f7406caaad34bfab802a10b740c5cc1
patryq03 <138616976+patryq03@users.noreply.github.com>
2023-12-16 18:00:54 +0100

    feat: change src folder

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,7 +7,28 @@ player.on('timeupdate', throttle((event) => {
     localStorage.setItem('videoplayer-current-time', event.seconds);
 },1000));
 
-window.addEventListener('DOMContentLoaded', () => {
+/*window.addEventListener('DOMContentLoaded', () => {
   const time = localStorage.getItem('videoplayer-current-time');
   player.setCurrentTime(time);
 });
+*/
+const storedTime = localStorage.getItem('videoplayer-current-time');
+
+if (storedTime) {
+  player
+    .setCurrentTime(storedTime)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
